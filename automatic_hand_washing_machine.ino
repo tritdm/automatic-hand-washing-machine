b2c74eaaf2374b4c99a3f8fbc255352cc20a7d98
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-11 11:14:59 +0700

    Update automatic_hand_washing_machine.ino

--- automatic_hand_washing_machine.ino
+++ automatic_hand_washing_machine.ino
@@ -1,85 +1,83 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define NhietDoPin A0
-#define BomPin 7
-#define HongNgoaiPin 8
-#define CoiPin 13
+#define Temperature_pin A0
+#define Pump_pin 7
+#define IR_pin 8
+#define Buzzer_pin 13
 float ran[] = {36.5, 36.6, 36.7, 36.8, 36.9, 37.1, 37.2, 37.3, 37.4, 37.5, 37.6, 37.7, 37.8, 37.9, 38.1, 38.2, 38.3, 38.4, 38.5};
 LiquidCrystal_I2C LCD (0x27,16,2);
 
-unsigned long Timer, HienThi;
-unsigned long BatDauBom, BatDauKiemTra, PastCheck;
+unsigned long Timer;
+unsigned long Start_pump, Start_check;
 unsigned long ThoiGianKiemTra = 4000; //4s
 
-float nhietdo;
-int hongngoai;
+float temperature;
+int IR;
 bool HandFlag = false;
-bool FlagBom = false;
+bool PumpFlag = false;
 
-void DocHongNgoai()
+void IR_read()
 {
-  hongngoai = digitalRead(HongNgoaiPin);
-  delay(50);
+    IR = digitalRead(IR_pin);
+    delay(50);
 }
 
-bool CoTayDuaVao()
+bool isHandThere()
 {
-  if (hongngoai == 0)
-  {      
-       return true;
-       HandFlag = true;
-  }
-  
-  else if (hongngoai != 0)
-  {
-      return false;
-      HandFlag = false;
-  }
+    if (IR == 0)
+    {      
+        return true;
+        HandFlag = true;
+    }
+
+    else if (IR != 0)
+    {
+        return false;
+        HandFlag = false;
+    }
 }
 
-void BatBom()
+void Pump_on()
 {
-  digitalWrite(BomPin, HIGH); 
-  digitalWrite(CoiPin, LOW); 
-  Serial.println("Bat bom");
+    digitalWrite(Pump_pin, HIGH); 
+    digitalWrite(Buzzer_pin, LOW); 
+    Serial.println("Bat bom");
 }
 
-void TatBom()
+void Pump_off()
 {
-  digitalWrite(BomPin, LOW);
-  digitalWrite(CoiPin, HIGH);
-  Serial.println("Tat bom");
+    digitalWrite(Pump_pin, LOW);
+    digitalWrite(Buzzer_pin, HIGH);
+    Serial.println("Tat bom");
 }
 
-void DocNhietDo()
+void Temp_read()
 {
-  int reading = analogRead(NhietDoPin);  
-  float voltage = reading * 5.0 / 1024.0;
-  nhietdo = voltage * 100.0;
+    int reading = analogRead(Temperature_pin);  
+    float voltage = reading * 5.0 / 1024.0;
+    temperature = voltage * 100.0;
 }
 
-void HienThiNhietDo()
+void Temp_show()
 {
-  int i;
-  //LCD.clear();
-  LCD.setCursor(0,0);
-  LCD.print("Nhiet do: ");
-  LCD.setCursor(12,1);
-  if (nhietdo >= 36.1 && nhietdo <= 37.9) LCD.print(nhietdo);
-  else
-  { 
-    int i = random(0,18);
-    nhietdo = ran[i];
-    LCD.print(nhietdo);
-  }
-  Serial.println(i);
-  Serial.println(nhietdo);
+    int i;
+    LCD.setCursor(0,0);
+    LCD.print("Nhiet do: ");
+    LCD.setCursor(12,1);
+    if (temperature >= 36.1 && temperature <= 37.9) LCD.print(temperature);
+    else
+    { 
+      i = random(0,18);
+      temperature = ran[i];
+      LCD.print(temperature);
+    }
+    Serial.println(i);
+    Serial.println(temperature);
 }
 
-void ManHinhCho()
+void Wait_screen()
 {
-    //LCD.clear();
     LCD.setCursor(0,0);
     LCD.print("Dua tay vao de");
     LCD.setCursor(4,1);
@@ -88,61 +86,57 @@ void ManHinhCho()
 
 void CheckTime()
 {
- LCD.clear();
- Timer = millis();
- while (millis() - BatDauBom < 500 ) HienThiNhietDo();
-  TatBom();
-  LCD.clear();
-  while (millis() - Timer < 3000){
-  HienThiNhietDo();
-  //Serial.println(")
-  }
+    LCD.clear();
+    Timer = millis();
+    while (millis() - Start_pump < 500 ) Temp_show();
+    Pump_off();
+    LCD.clear();
+    while (millis() - Timer < 3000)
+    {
+        Temp_show();
+    }
 }
 
 void setup(){
-  Serial.begin(9600);
-  LCD.backlight();
-  LCD.begin(16,2);
-  LCD.setCursor(0,0);
-  LCD.clear();
-  pinMode(NhietDoPin, INPUT);
-  pinMode(HongNgoaiPin, INPUT);
-  pinMode(BomPin, OUTPUT);
-  LCD.setCursor(0,0);
-  LCD.print("Dua tay vao de");
-  LCD.setCursor(4,1);
-  LCD.print("do nhiet do!");
+    Serial.begin(9600);
+    LCD.backlight();
+    LCD.begin(16,2);
+    LCD.setCursor(0,0);
+    LCD.clear();
+    pinMode(Temperature_pin, INPUT);
+    pinMode(IR_pin, INPUT);
+    pinMode(Pump_pin, OUTPUT);
+    LCD.setCursor(0,0);
+    LCD.print("Dua tay vao de");
+    LCD.setCursor(4,1);
+    LCD.print("do nhiet do!");
 }
 
 void loop()
 {
-  // Serial.println("Start");
-  DocHongNgoai();
-  //Serial.println(hongngoai);
-  
-  if (CoTayDuaVao())
-  {
-  Serial.println("Co Tay");
-  DocNhietDo();
-  HienThiNhietDo();
-  if (FlagBom == false)
-  {
-  BatDauBom = millis();
-  FlagBom = true;
-  }
-  BatBom();  
-  CheckTime();  
-  HienThi = millis();
-  }
-  else 
-  {
-  FlagBom = false;
-  if (!CoTayDuaVao())
-  {
-    Serial.println("Khong Co Tay");
-    TatBom();
-    ManHinhCho();
-    delay(50);
-  }
-  }
+    IR_read();
+    if (isHandThere())
+    {
+        Serial.println("Co Tay");
+        Temp_read();
+        Temp_show();
+        if (PumpFlag == false)
+        {
+            Start_pump = millis();
+            PumpFlag = true;
+        }
+        Pump_on();  
+        CheckTime();
+    }
+    else 
+    {
+        PumpFlag = false;
+        if (!isHandThere())
+        {
+            Serial.println("Khong Co Tay");
+            Pump_off();
+            Wait_screen();
+            delay(50);
+        }
+    }
 }
